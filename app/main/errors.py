a998e4795125b0c860c09a547469a85673c4af5e
doaa-altarawy <doaa.altarawy@gmail.com>
2019-02-27 16:22:33 -0500

    Fix error handling

--- app/main/errors.py
+++ app/main/errors.py
@@ -13,8 +13,9 @@ def not_found(error):
 
     if request.path.startswith('/api/'):
         return jsonify(error_data), 404
-    return render_template('404.html', error_data=error_data, formats=data_loader.formats,
-                           help_data=data_loader.help_data), 404
+    return render_template('404.html',
+                           error_data=error_data,
+                           formats=data_loader.formats), 404
 
 
 @main.app_errorhandler(500)
@@ -26,5 +27,6 @@ def bse_library_exception(error):
 
     if request.path.startswith('/api/'):
         return jsonify(error_data), 500
-    return render_template('error.html', error_data=error_data, formats=data_loader.formats,
-                           help_data=data_loader.help_data), 500
+    return render_template('error.html',
+                           error_data=error_data,
+                           formats=data_loader.formats), 500
