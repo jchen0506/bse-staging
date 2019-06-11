82e3d5c98e73192ee744884925016ad8ca122b88
doaa-altarawy <doaa.altarawy@gmail.com>
2019-06-11 14:26:18 -0400

    extract extra geo location from geoip2

---
+++ app/models/geo_location_util.py
@@ -0,0 +1,33 @@
+"""
+This product includes GeoLite2 data created by MaxMind, available from
+https://www.maxmind.com
+(attribution requirement)
+"""
+
+import geoip2.database
+from os.path import dirname, join, abspath
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+_app_path = dirname(dirname(abspath(__file__)))
+_geo_file = "GeoLite2-City.mmdb"
+geoip2_reader = geoip2.database.Reader(join(_app_path, 'static', 'data', 'geo',
+                                            'GeoLite2-City_latest', _geo_file))
+
+
+def get_geoip2_data(ip_address):
+    out = {}
+    try:
+        loc_data = geoip2_reader.city(ip_address)
+        out['city'] = loc_data.city.name
+        out['country'] = loc_data.country.name
+        out['country_code'] = loc_data.country.iso_code
+        out['ip_lat'] = loc_data.location.latitude
+        out['ip_long'] = loc_data.location.longitude
+        out['postal_code'] = loc_data.postal.code
+    except:
+        logger.error('Problem getting geoip data for {}'.format(ip_address))
+
+    return out
